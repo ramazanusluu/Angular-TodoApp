3e5e5e90865f045c331bebd82ed0af6e4bd66c81
Ramazan USLU <ruslu1720@gmail.com>
2022-08-04 18:00:19 +0300

    Templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  //selector: 'app-root',
-  //selector: '.app-root',
-  // selector: '#app-root',
   selector: 'app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  template: `
+    <h1>Angular App Component</h1>
+    <h2>React vs Angular</h2>
+    <h3>{{ title }}</h3>
+    <h3>{{ getTitle() }}</h3>
+    <div>{{ todoItem.description }} - {{ todoItem.action }}</div>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todoApp';
+  title = 'todo app';
+  todoItem = {
+    description: 'deneme',
+    action: true,
+  };
+  getTitle() {
+    return this.title;
+  }
 }
