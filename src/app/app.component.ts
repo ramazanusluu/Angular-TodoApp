38c96dd8c437264620307c2bfe9167cb2cde234a
Ramazan USLU <ruslu1720@gmail.com>
2022-08-04 17:38:38 +0300

    component selectors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  //selector: 'app-root',
+  //selector: '.app-root',
+  // selector: '#app-root',
+  selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
