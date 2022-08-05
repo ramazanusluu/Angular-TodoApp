1802640ff429e5eeca634fec8e8ed7d9733c712d
Ramazan USLU <ruslu1720@gmail.com>
2022-08-05 08:16:04 +0300

    Listeler

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
-export class TodoComponent implements OnInit {
+export class TodoComponent {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  private name = 'Ramazan';
+  // items = ['item 1', 'item 2', 'item 3', 'item 4'];
+  items = [
+    {
+      id: 1,
+      description: 'Kahvaltı',
+      action: 'yes',
+    },
+    {
+      id: 2,
+      description: 'Spor',
+      action: 'yes',
+    },
+    {
+      id: 3,
+      description: 'Alışveriş',
+      action: 'no',
+    },
+  ];
+  getName() {
+    return this.name;
   }
-
 }
